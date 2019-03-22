3a5cc141ebf8cc0aa6c93051cab649c2a613d238
Bykovskikh Evgeny <e.bykovskikh@rambler-co.ru>
2019-03-22 03:40:35 +0300

    Add CompareUtility for files compare

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace xLaunch
@@ -9,15 +10,28 @@ namespace xLaunch
     static async Task Main(string[] args)
     {
       List<LaunchFile> localFiles = Utility.getLocalFiles();
-      foreach (LaunchFile localFile in localFiles)
+      List<LaunchFile> serverFiles = await Utility.getServerFiles();
+
+      List<LaunchFile> extraFiles = CompareUtility.getExtraFiles(localFiles, serverFiles);
+      List<LaunchFile> wrongFiles = CompareUtility.getWrongFiles(localFiles, serverFiles);
+      List<LaunchFile> missingFiles = CompareUtility.getMissingFiles(localFiles, serverFiles);
+
+      Console.WriteLine("=== extra files (delete) ===");
+      foreach (LaunchFile extraFile in extraFiles)
       {
-        Console.WriteLine(localFile.filename + " : " + localFile.hash);
+        Console.WriteLine(extraFile.filename + " : " + extraFile.hash);
       }
 
-      List<LaunchFile> serverFiles = await Utility.getServerFiles();
-      foreach (LaunchFile serverFile in serverFiles)
+      Console.WriteLine("=== missing files (download) ===");
+      foreach (LaunchFile missingFile in missingFiles)
       {
-        Console.WriteLine(serverFile.filename + " : " + serverFile.hash);
+        Console.WriteLine(missingFile.filename + " : " + missingFile.hash);
+      }
+
+      Console.WriteLine("=== wrong files (replace) ===");
+      foreach (LaunchFile wrongFile in wrongFiles)
+      {
+        Console.WriteLine(wrongFile.filename + " : " + wrongFile.hash);
       }
     }
   }
---
+++ Utility/CompareUtility.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using xLaunch;
+
+internal class CompareUtility
+{
+  public static List<LaunchFile> getWrongFiles(List<LaunchFile> localFiles, List<LaunchFile> serverFiles)
+  {
+    List<LaunchFile> wrongFiles = new List<LaunchFile>();
+
+    foreach (LaunchFile localFile in localFiles)
+    {
+      LaunchFile serverSignature = serverFiles.Find(x => x.filename == localFile.filename);
+
+      if (serverSignature != null && serverSignature.hash != localFile.hash)
+      {
+        wrongFiles.Add(serverSignature);
+      }
+    }
+
+    return wrongFiles;
+  }
+
+  public static List<LaunchFile> getMissingFiles(List<LaunchFile> localFiles, List<LaunchFile> serverFiles)
+  {
+    List<LaunchFile> missingFiles = new List<LaunchFile>();
+
+    foreach (LaunchFile serverFile in serverFiles)
+    {
+      LaunchFile localSignature = localFiles.Find(x => x.filename == serverFile.filename);
+
+      if (localSignature == null)
+      {
+        missingFiles.Add(serverFile);
+      }
+    }
+
+    return missingFiles;
+  }
+
+  public static List<LaunchFile> getExtraFiles(List<LaunchFile> localFiles, List<LaunchFile> serverFiles)
+  {
+    List<LaunchFile> extraFiles = new List<LaunchFile>();
+
+    foreach (LaunchFile localFile in localFiles)
+    {
+      LaunchFile serverSignature = serverFiles.Find(x => x.filename == localFile.filename);
+
+      if (serverSignature == null)
+      {
+        extraFiles.Add(localFile);
+      }
+    }
+
+    return extraFiles;
+  }
+}
